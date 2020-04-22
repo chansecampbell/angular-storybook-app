c006b87bcacb1946c59f05f581b0c94e5f84bc7d
Chanse Campbell <chansecampbell@gmail.com>
2020-04-22 12:41:14 +0100

    update.

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Button } from "carbon-components-angular";
 
 @Component({
   selector: 'app-button',
-  templateUrl: './button.component.html',
   styleUrls: ['./button.component.css']
 })
-export class ButtonComponent implements OnInit {
 
-  constructor() { }
+export class ButtonComponent extends Button implements OnInit {
+  constructor() { 
+    super()
+  }
 
   ngOnInit(): void {
   }
-
 }
+
+
--- src/app/button/button.module.ts
+++ src/app/button/button.module.ts
@@ -1,13 +1,11 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
-import { ButtonModule } from "carbon-components-angular";
-
 import { ButtonComponent } from "./button.component";
 
 @NgModule({
   declarations: [ButtonComponent],
-  imports: [BrowserModule, ButtonModule],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [ButtonComponent]
 })
