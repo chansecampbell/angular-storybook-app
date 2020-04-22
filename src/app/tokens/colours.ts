a923a731c4c7d856664b7bcd7fe62fd42dfcaa62
Chanse Campbell <chansecampbell@gmail.com>
2020-04-22 10:52:03 +0100

    Updated colours.

---
+++ src/app/tokens/colours.ts
@@ -0,0 +1,12 @@
+export default {
+    interactive01: "#BA0AC1",
+    interactive02: "#393939",
+    hoverPrimary: "#910297",
+    hoverSecondary: "#4C4C4C",
+    activePrimary: "#E70EF0",
+    activeSecondary: "#6F6F6F",
+    disabled02: "#C6C6C6",
+    disabled03: "#8D8D8D",
+    text04: "#ffffff",
+    focus: "#0F62FE"
+}
---
+++ src/stories/0-guidelines/tokens/documentation.md
@@ -0,0 +1,10 @@
+# Button
+
+The PSW design system mainly uses two kinds of buttons, Primary and Secondary.
+
+To view the full documentation or see sample implementations be sure to check out the official docs.
+
+Useful links:
+---
+
+[Button Documentation](https://www.carbondesignsystem.com/components/button/usage)
